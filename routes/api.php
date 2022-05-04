9c7bf4cc24504c8e73a86c2c97d375976d608b0d
Mateus M. Côrtes <mmendescortes@gmail.com>
2022-05-04 16:02:52 -0300

    fix-cors

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use App\Http\Controllers\ElectroController;
 |
 */
 
-Route::resource('electro', ElectroController::class);
+Route::middleware(['cors'])->resource('electro', ElectroController::class);
